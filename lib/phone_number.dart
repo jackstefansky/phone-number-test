6fc649c2afb06bda4e30f4fd26a7f3678a67b1a3
jackstefansky <stefphan01@gmail.com>
2020-05-20 15:33:37 +0200

    reformat

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,82 +1,45 @@
 class PhoneNumber {
-  String clean(String number) {
-    String phoneNumber = number;
+  RegExp _nonNumbersChars = RegExp(r'(\D)');
+  RegExp _punctuation = RegExp(r'([,\/#!$%\^&\*;:{}=\_`~])');
+  RegExp _letters = RegExp(r'([A-Za-z])');
 
-    phoneNumber = _cleanNumber(phoneNumber);
-    phoneNumber = _checkLength(phoneNumber);
-    phoneNumber = _checkIfNumbersOnly(phoneNumber);
-    phoneNumber = _checkArea(phoneNumber);
+  String clean(String phoneNumber) {
+    _checkIfCorrect(phoneNumber);
+    String cleanedNumber = phoneNumber.replaceAll(_nonNumbersChars, "");
+    _checkIfProperLength(cleanedNumber);
 
-    return phoneNumber;
-  }
-
-  String _cleanNumber(String number) {
-    String phoneNumber = number;
-    //I'm not sure why this doesn't work
-    //RegExp charsToReplace = RegExp('[ -.()]');
-    //phoneNumber = phoneNumber.replaceAll(charsToReplace, '');
-
-    phoneNumber = phoneNumber.replaceAll('.', '');
-    phoneNumber = phoneNumber.replaceAll(' ', '');
-    phoneNumber = phoneNumber.replaceAll('(', '');
-    phoneNumber = phoneNumber.replaceAll(')', '');
-    phoneNumber = phoneNumber.replaceAll('-', '');
-    phoneNumber = phoneNumber.replaceAll('+', '');
-
-    return phoneNumber;
-  }
-
-  String _checkLength(String number) {
-    String phoneNumber = number;
-    if (phoneNumber.length < 10) {
-      throw FormatException("incorrect number of digits");
-    } else if (phoneNumber.length > 11) {
-      throw FormatException("more than 11 digits");
-    } else if (phoneNumber.length == 11) {
-      if (!phoneNumber.startsWith('1')) {
-        throw FormatException("11 digits must start with 1");
-      } else {
-        return phoneNumber.substring(1);
-      }
-    } else {
-      return phoneNumber;
-    }
-  }
-
-  String _checkIfNumbersOnly(String number) {
-    String phoneNumber = number;
-
-    if (phoneNumber.contains(
-      RegExp(r'[a-zA-Z]'),
-    )) throw FormatException("letters not permitted");
-
-    if (phoneNumber.contains(
-      RegExp(r'[@:!]'),
-    )) throw FormatException("punctuations not permitted");
-
-    return phoneNumber;
-  }
-
-  String _checkArea(String number) {
-    String phoneNumber = number;
-
-    if (phoneNumber[0] == '0' ||
-        phoneNumber[0] == '1' ||
-        phoneNumber[3] == '0' ||
-        phoneNumber[3] == '1') {
-      if (phoneNumber[0] == '0') {
-        throw FormatException("area code cannot start with zero");
-      }
-      if (phoneNumber[0] == '1') {
-        throw FormatException("area code cannot start with one");
-      }
-      if (phoneNumber[3] == '0') {
-        throw FormatException("exchange code cannot start with zero");
-      } else {
-        throw FormatException("exchange code cannot start with one");
-      }
+    if (cleanedNumber.length == 11) {
+      if (cleanedNumber[0] != '1')
+        throw FormatException('11 digits must start with 1');
+      else
+        cleanedNumber = cleanedNumber.substring(1);
     }
 
-    return phoneNumber;
+    _checkAreaAndExchangeCode(cleanedNumber);
+    return cleanedNumber;
+  }
+
+  void _checkIfProperLength(String phoneNumber) {
+    if (phoneNumber.length < 10)
+      throw FormatException('incorrect number of digits');
+    if (phoneNumber.length > 11) throw FormatException('more than 11 digits');
+  }
+
+  void _checkIfCorrect(String number) {
+    if (_punctuation.hasMatch(number))
+      throw new FormatException('punctuations not permitted');
+    if (_letters.hasMatch(number))
+      throw new FormatException('letters not permitted');
+  }
+
+  void _checkAreaAndExchangeCode(String number) {
+    if (number[0] == '0')
+      throw FormatException('area code cannot start with zero');
+    if (number[0] == '1')
+      throw FormatException('area code cannot start with one');
+    if (number[3] == '0')
+      throw FormatException('exchange code cannot start with zero');
+    if (number[3] == '1')
+      throw FormatException('exchange code cannot start with one');
   }
 }
